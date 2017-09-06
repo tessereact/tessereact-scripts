7e1f409deb5ad70ae4186b009cc74131b5bff3d0
Lesha Koss <regiusprod@gmail.com>
2017-09-06 18:20:03 +0600

    Replace multi-entry-loader with virtual-module-webpack-plugin

--- config/webpack.config.tessereact.js
+++ config/webpack.config.tessereact.js
@@ -1,20 +1,41 @@
 const devConfig = require('react-scripts/config/webpack.config.dev')
 const fs = require('fs')
 const path = require('path')
-const glob = require('glob')
+const VirtualModulePlugin = require('virtual-module-webpack-plugin')
 
 const appDirectory = fs.realpathSync(process.cwd());
+const srcDirectory = path.join(appDirectory, './src')
+
+const tessereactEntry = `
+const cssContext = require.context('${srcDirectory}', true, ${/.css$/})
+cssContext.keys().forEach(cssContext)
+
+const scenariosContext = require.context('${srcDirectory}', true, ${/\/scenarios\.jsx?$/})
+scenariosContext.keys().forEach(scenariosContext)
+`
 
 const testshotConfig = Object.assign({}, devConfig, {
   entry: [
     ...devConfig.entry.slice(0, -1),
-    `multi-entry-loader?include[]=${appDirectory}/src/**/*.css,include[]=${appDirectory}/src/**/scenarios.js,include[]=${appDirectory}/src/**/scenarios.jsx!`,
+
+    // A virtual file which imports all .css and scenarios.jsx files from src/ directory
+    path.join(appDirectory, 'tessereactEntry.js'),
+
+    // Actually run the tessereact UI
     require.resolve('tessereact/entry')
   ],
 
   output: Object.assign({}, devConfig.output, {
-    filename: 'static/js/testshot.js'
-  })
+    filename: 'static/js/tessereact.js'
+  }),
+
+  plugins: [
+    new VirtualModulePlugin({
+      moduleName: path.join(appDirectory, 'tessereactEntry.js'),
+      contents: tessereactEntry
+    }),
+    ...devConfig.plugins
+  ]
 })
 
 module.exports = testshotConfig
