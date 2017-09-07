8c7f0e5ba53c7512225c5a0bd7cfdc0ef3718dc6
Lesha Koss <regiusprod@gmail.com>
2017-09-07 16:09:21 +0600

    Remove note about PATENTS file

--- bin/tessereact-start.js
+++ bin/tessereact-start.js
@@ -5,8 +5,7 @@
  * All rights reserved.
  *
  * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
+ * LICENSE file in the root directory of this source tree.
  */
 
 // Do this as the first thing so that any code reading it knows the right env.
