0995db7ebf5b98ccf11baae381543173401710e1
Lesha Koss <regiusprod@gmail.com>
2017-09-07 16:08:40 +0600

    Change license to BSD-3-Clause

--- bin/tessereact-start.js
+++ bin/tessereact-start.js
@@ -1,5 +1,14 @@
 #!/usr/bin/env node
 
+/**
+ * Copyright (c) 2017-present, Facebook, Inc. and Tessereact authors.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'development'
 process.env.NODE_ENV = 'development'
